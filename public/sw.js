954e6750709f41d922f05b598fa2a5f10bc48203
govgranter <luckyiphonegrab@gmail.com>
2025-12-01 20:12:01 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'starmart';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install event - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
